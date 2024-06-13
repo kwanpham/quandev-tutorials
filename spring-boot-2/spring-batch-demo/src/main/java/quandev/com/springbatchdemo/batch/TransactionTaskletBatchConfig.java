fac239056989a9e9a73f7565c3822c49a09a6d1a
quandev <chomchomcapital@gmail.com>
2024-06-13 22:29:00 +0700

    Auto stash before checking out "origin/master"

--- spring-boot-2/spring-batch-demo/src/main/java/quandev/com/springbatchdemo/batch/TransactionTaskletBatchConfig.java
+++ spring-boot-2/spring-batch-demo/src/main/java/quandev/com/springbatchdemo/batch/TransactionTaskletBatchConfig.java
@@ -2,9 +2,13 @@ package quandev.com.springbatchdemo.batch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,7 +38,7 @@ public class TransactionTaskletBatchConfig {
     protected Step readTransactionLines() {
         return steps
                 .get("readLines")
-                .tasklet(new LinesReader())
+                .tasklet((stepContribution, chunkContext) -> null)
                 .build();
     }
 
